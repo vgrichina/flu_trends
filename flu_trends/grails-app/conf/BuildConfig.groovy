7141fd4e75ec3bf01491d4f046aed92f0f63b70d
Vladimir Grichina <vgrichina@gmail.com>
2009-11-23 03:51:24 +0200

    Added dependencies to Hibernate Spatial

--- flu_trends/grails-app/conf/BuildConfig.groovy
+++ flu_trends/grails-app/conf/BuildConfig.groovy
@@ -5,22 +5,26 @@ grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.dependency.resolution = {
     inherits "global" // inherit Grails' default dependencies
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    repositories {        
+    repositories {
         grailsPlugins()
         grailsHome()
 
         // uncomment the below to enable remote dependency resolution
         // from public Maven repositories
         //mavenLocal()
-        //mavenCentral()
+        mavenCentral()
         //mavenRepo "http://snapshots.repository.codehaus.org"
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
+        mavenRepo "http://maven.geotools.fr/repository"
+        mavenRepo "http://www.hibernatespatial.org/repository"
     }
     dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+        // supecify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
         // runtime 'mysql:mysql-connector-java:5.1.5'
+        runtime "mysql:mysql-connector-java:5.1.9",
+                "org.hibernatespatial:hibernate-spatial-mysql:1.0-SNAPSHOT"
     }
 
 }
