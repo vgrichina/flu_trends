634c4943ba2430d15882c326b40937481902603a
Vladimir Grichina <vgrichina@gmail.com>
2009-11-25 03:34:29 +0200

    Added entity to store Twitter messages with their location

---
+++ flu_trends/grails-app/domain/Message.groovy
@@ -0,0 +1,17 @@
+import com.vividsolutions.jts.geom.Point  
+
+class Message {
+    String user
+    String imageUrl
+    String status
+    Point location
+    boolean processed = false
+
+    static constraints = {
+        user(nullable: false, blank: false, maxSize: 20)
+        imageUrl(nullable: true, blank: false, maxSize: 256)
+        status(nullable: false, blank: false, maxSize: 140)
+        location(nullable: true)
+        processed(nullable: false)
+    }
+}
