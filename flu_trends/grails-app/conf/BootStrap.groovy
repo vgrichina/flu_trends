52be887ed1eb8ff870a2e3b764ac82be0967ae5b
Vladimir Grichina <vgrichina@gmail.com>
2009-11-30 03:24:39 +0200

    Added default search for "swine flu"

--- flu_trends/grails-app/conf/BootStrap.groovy
+++ flu_trends/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,11 @@
 class BootStrap {
 
      def init = { servletContext ->
+        // Create default search for "swine flu"
+        new MessageSearch(query: "swine flu").save(failOnError: true)
      }
+     
      def destroy = {
      }
-} 
+}
+
