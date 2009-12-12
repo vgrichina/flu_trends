b02ae1c6871402a456d5a5bf7c8f4ba2c27a47a9
Vladimir Grichina <vgrichina@gmail.com>
2009-12-12 03:59:15 +0200

    Initial implementation of retrieving the location of users

--- flu_trends/grails-app/jobs/GetMessageLocationsJob.groovy
+++ flu_trends/grails-app/jobs/GetMessageLocationsJob.groovy
@@ -1,4 +1,6 @@
+import com.vividsolutions.jts.geom.Coordinate
 import com.vividsolutions.jts.geom.Point
+import com.vividsolutions.jts.geom.GeometryFactory
 
 class GetMessageLocationsJob {
     /*
@@ -12,17 +14,53 @@ class GetMessageLocationsJob {
     def concurrent = false
 
     /*
-     * TwitterService instance used to access Twitter API
+     * TwitterService instance used to access Twitter API.
      */
     def twitterService
 
     /*
+     * LocationService instance used to access Geocoding API.
+     */
+    def locationService
+
+    /*
+     * A factory used to create geomertry objects.
+     */
+    def geometryFactory = new GeometryFactory()
+
+    /*w
      * Executes task
      */
     def execute() {
         // Find all the messages without location
-        def messages = Message.findAllByLocationIsNull()
-        
-        messages.each { println it.text }
+        // TODO: There can be very big list, limit results
+        def messages
+        Message.withTransaction {
+            messages = Message.findAllByLocationIsNull()
+        }
+
+        // Simple cache for user locations
+        def userLocationCache = [:]
+
+        // For all of the messages try to determine location
+        messages.each { message ->
+            // If there is no cached location, it needs to be retrieved at first
+            if (!userLocationCache[message.user]) {
+                // Fetch info about user
+                def user = twitterService.show(message.user)
+                def coords = locationService.getCoordsFromLocation(user.location)
+                if (coords) {
+                    userLocationCache[message.user] =
+                        geometryFactory.createPoint(new Coordinate(coords.latitude, coords.longitude))
+                }
+            }
+            Message.withTransaction {
+                // Set message location, make message as processed, and save it
+                message.location = userLocationCache[message.user]
+                println "location = ${message.location}"
+                message.processed = true
+                message.save(failOnError: true)
+            }
+        }
     }
 }
